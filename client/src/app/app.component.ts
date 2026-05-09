46c182b13f74166bb1d69940e9abe47af0d885d7
SaraswathiS2004 <saraswathis1332004@gmail.com>
2026-05-10 00:36:24 +0530

    Adnin with google sheet integration

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit, OnDestroy } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { TranslateService, Lang } from './services/translate.service';
 import { TranslatePipe } from './pipes/translate.pipe';
+import { AdminService, SheetSyncStatus } from './services/admin.service';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +12,37 @@ import { TranslatePipe } from './pipes/translate.pipe';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
 
   showInvMenu = false;
+  syncStatus: SheetSyncStatus | null = null;
 
-  constructor(public translateService: TranslateService) {}
+  private syncPollInterval: any;
+
+  constructor(
+    public translateService: TranslateService,
+    private adminService: AdminService
+  ) {}
+
+  ngOnInit(): void {
+    this.pollSyncStatus();
+    this.syncPollInterval = setInterval(() => this.pollSyncStatus(), 30000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.syncPollInterval);
+  }
+
+  private pollSyncStatus(): void {
+    this.adminService.getSheetStatus().subscribe({
+      next: (s) => { this.syncStatus = s; },
+      error: () => {}
+    });
+  }
+
+  get showSyncWarning(): boolean {
+    return !!(this.syncStatus?.configured && !this.syncStatus.lastSyncSuccess);
+  }
 
   toggleInvMenu(): void {
     this.showInvMenu = !this.showInvMenu;
---
+++ client/src/app/services/admin.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface TableSyncInfo {
+  success: boolean;
+  error: string | null;
+  timestamp: number;
+}
+
+export interface SheetSyncStatus {
+  configured: boolean;
+  spreadsheetId: string | null;
+  spreadsheetUrl: string | null;
+  lastSyncSuccess: boolean;
+  lastSyncError: string | null;
+  lastSyncTimestamp: number;
+  tableStatuses: { [tableName: string]: TableSyncInfo };
+}
+
+export interface AdminApiMessage {
+  status: 'SUCCESS' | 'FAIL';
+  message: string;
+  spreadsheetId?: string;
+  spreadsheetUrl?: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class AdminService {
+
+  private baseUrl = 'api/admin';
+
+  constructor(private http: HttpClient) {}
+
+  getSheetStatus(): Observable<SheetSyncStatus> {
+    return this.http.get<SheetSyncStatus>(`${this.baseUrl}/sheet-status`);
+  }
+
+  setupSheet(payload: {
+    action: 'create' | 'link';
+    credentialsFileName: string;
+    title?: string;
+    spreadsheetId?: string;
+  }): Observable<AdminApiMessage> {
+    return this.http.post<AdminApiMessage>(`${this.baseUrl}/sheet-setup`, payload);
+  }
+
+  restoreFromSheet(): Observable<AdminApiMessage> {
+    return this.http.post<AdminApiMessage>(`${this.baseUrl}/restore-from-sheet`, {});
+  }
+
+  syncNow(): Observable<AdminApiMessage> {
+    return this.http.post<AdminApiMessage>(`${this.baseUrl}/sync-now`, {});
+  }
+}
